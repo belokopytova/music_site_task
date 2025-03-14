1770ef4f62e785ccb744a0626d223112b5003666
Anna Belokopytova <belo.anna.a91@gmail.com>
2025-03-14 13:37:50 +0300

    Added select_Script.sql

---
+++ select_Script.sql
@@ -0,0 +1,61 @@
+--Задание №2
+
+--Название и продолжительность самого длительного трека
+SELECT name, duration FROM songs
+WHERE duration = (SELECT MAX(duration) FROM songs);
+
+--Название треков, продолжительность которых не менее 3,5 минут
+SELECT name FROM songs
+WHERE duration >= '3:30:00';
+
+
+--Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+SELECT name FROM collections
+WHERE year <= 2020 AND year >= 2018;
+
+
+--Исполнители, чьё имя состоит из одного слова
+SELECT name FROM singers
+WHERE name NOT LIKE '% %';
+
+--Название треков, которые содержат слово «мой» или «my»
+SELECT name FROM songs
+WHERE name LIKE 'My _%' OR name LIKE 'Мой _%'
+OR name LIKE 'My' OR name LIKE 'Мой'
+OR name LIKE '_% my _%' OR name LIKE '_% мой _%'
+OR name LIKE '_% my' OR name LIKE '_% мой';
+
+
+--Задание №3
+
+--Количество исполнителей в каждом жанре
+SELECT name, COUNT(singerid) FROM genres g       
+JOIN singergenre s ON s.genreid = g.id   
+GROUP BY g.name;
+
+--Количество треков, вошедших в альбомы 2019–2020 годов.
+SELECT COUNT(name) FROM songs s
+LEFT JOIN albums a ON a.id=s.album_id
+WHERE album_id IN (SELECT id FROM albums WHERE year >=2019 AND year<=2020);
+
+--Средняя продолжительность треков по каждому альбому
+SELECT a.title, AVG(duration) FROM albums a 
+JOIN songs s on a.id = s.album_id
+GROUP BY a.title;
+
+--Все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT name FROM singers s 
+JOIN albumsinger a ON a.singerid=s.id
+JOIN albums al ON al.id=a.albumid
+WHERE al.year <> 2020;
+
+--Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
+SELECT c.name from collections c 
+JOIN collectionsong c2 ON c.id=c2.collectionid
+JOIN songs s ON s.id=c2.songid
+JOIN albums a ON a.id=s.album_id
+JOIN albumsinger al ON al.albumid=a.id
+JOIN singers si ON si.id = al.singerid
+WHERE si.name = 'Баста';
+
+
